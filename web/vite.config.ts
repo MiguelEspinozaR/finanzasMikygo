e86a04328f63bd5dbcd66fd0ffa5090c418bd9cb
Miky <miky@finanzasmiky.com>
2026-07-21 14:48:51 -0400

    vite fixed again

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -11,7 +12,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['hudson-convicted-gene-patricia.trycloudflare.com'],
+    allowedHosts: 'all',
     port: 5173,
     proxy: {
       '/api': {
